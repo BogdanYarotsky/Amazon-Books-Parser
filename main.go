880fcef37852770415090273367ba4a37560da58
Bogdan Yarotsky <byarotsky@gmail.com>
2021-11-09 21:31:27 +0200

    Added http server foundations

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"html/template"
+	"log"
+	"net/http"
+	"regexp"
 )
 
-const sample_query = "python"
+type TopplerResult struct {
+	Title string
+	Books []Book
+}
+
+var indexPath = regexp.MustCompile("/.")
+var resultPath = regexp.MustCompile("^/(results)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+// for static files
+func serveStaticFiles() {
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	m := indexPath.FindStringSubmatch(r.URL.Path)
+
+	if m != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	p := &TopplerResult{Title: "Hello Toppler", Books: nil}
+	err := templates.ExecuteTemplate(w, "index.html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func main() {
-	var query string
+	serveStaticFiles()
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	// will be moved to the method for page results
 
-	if len(os.Args) == 2 {
-		query = os.Args[1]
-	} else {
-		query = sample_query
-	}
-
-	books, err := GetBooks(query)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Amazon top:")
-	PrintBooks(SortAmazonBooks(books)...)
-	fmt.Println("Goodreads top:")
-	PrintBooks(SortGoodreadsBooks(books)...)
+	//var query string
+	//if len(os.Args) == 2 {
+	//	query = os.Args[1]
+	//}
+	//books, err := GetBooks(query)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Amazon top:")
+	//PrintBooks(SortAmazonBooks(books)...)
+	//fmt.Println("Goodreads top:")
+	//PrintBooks(SortGoodreadsBooks(books)...)
 
 }
